c22621176cd91dd89f6bd374e268c97e984b1fe5
Slava Epifanov <slava.epifanov@gmail.com>
2018-06-05 14:48:40 +0300

    add initial code

---
+++ config/unicorn.rb
@@ -0,0 +1,16 @@
+#root = "/work/www/ucs-redmine/current"
+root = "/work/www/ucs-redmine/"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/ucs-redmine.sock"
+worker_processes 2
+timeout 30
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+    ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
